88717bd7cc1a6e8172dba1618742eaa4c9d468f9
Steven Muganwa <stevenmuganwa@gmail.com>
2019-08-27 16:08:15 +0300

    fixed track toggle and period at end of sentence

--- Misc/Strings.js
+++ Misc/Strings.js
@@ -11,7 +11,7 @@ export const footer = {
 export const author = {
     name: "Jim Flannery",
     intro: "I am a former high school teacher who became fed up with how poorly the system treats young people." +
-    " Join me on my mission to liberate young people from schools to thrive in freedom",
+    " Join me on my mission to liberate young people from schools to thrive in freedom.",
     callToAction: "Want updates about future projects?",
     buttonText: "Join mailing list",
     emailPlaceHolder: "email@provider.com"
