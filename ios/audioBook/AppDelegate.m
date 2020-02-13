4dbe58c50cb01d8dd0a5a5c35a52896dc4631750
Muganwa <stevenmuganwa@gmail.com>
2020-02-13 18:02:51 +0300

    ios compile post

--- ios/audioBook/AppDelegate.m
+++ ios/audioBook/AppDelegate.m
@@ -24,7 +24,7 @@
                                             initialProperties:nil];
   
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];// allow play in the background
+  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];// allow play without headsets
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
