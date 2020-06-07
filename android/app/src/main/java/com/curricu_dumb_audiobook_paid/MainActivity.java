925cd8488f418fc3391b3aaa0aaaa2abff7e9617
Muganwa <stevenmuganwa@gmail.com>
2020-06-07 20:37:55 +0300

    to paid

--- android/app/src/main/java/com/curricu_dumb_audiobook_paid/MainActivity.java
+++ android/app/src/main/java/com/curricu_dumb_audiobook_paid/MainActivity.java
@@ -1,4 +1,4 @@
-package com.curricu_dumb_audiobook;
+package com.curricu_dumb_audiobook_paid;
 
 import android.content.Intent;
 import android.util.Log;
--- android/app/src/main/java/com/curricu_dumb_audiobook_paid/MainApplication.java
+++ android/app/src/main/java/com/curricu_dumb_audiobook_paid/MainApplication.java
@@ -1,4 +1,4 @@
-package com.curricu_dumb_audiobook;
+package com.curricu_dumb_audiobook_paid;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
